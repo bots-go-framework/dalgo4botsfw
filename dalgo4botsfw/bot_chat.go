e7595bbae24d861a61fe7e6022cd797607b4c05d
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-04-06 14:57:30 +0100

    chore: unused struct commented out

--- dalgo4botsfw/bot_chat.go
+++ dalgo4botsfw/bot_chat.go
@@ -1,11 +1,6 @@
 package dalgo4botsfw
 
-import (
-	"github.com/bots-go-framework/bots-fw/botsfw"
-	"github.com/dal-go/dalgo/record"
-)
-
-type botChat struct {
-	record.WithID[string]
-	Data botsfw.BotChat
-}
+//type botChat struct {
+//	record.WithID[string]
+//	Data botsfw.BotChat
+//}
